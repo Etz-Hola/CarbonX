047fb00723ea59cca280b59c50e97b524bd421e3
Qadir Adesoye <abdqadirola@gmail.com>
2025-05-15 07:48:02 +0100

    update on frontend

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Dashboard from './pages/Dashboard';
+import Marketplace from './pages/Marketplace';
+import CreditDetails from './pages/CreditDetails';
+import Portfolio from './pages/Portfolio';
+import Impact from './pages/Impact';
+import YieldFarming from './pages/YieldFarming';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="marketplace" element={<Marketplace />} />
+        <Route path="marketplace/:id" element={<CreditDetails />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="impact" element={<Impact />} />
+        <Route path="yield" element={<YieldFarming />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
